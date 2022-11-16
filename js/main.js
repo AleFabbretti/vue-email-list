d93d60c59730cb09ff029a23aa79ce06dd02fae7
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-11-16 17:20:02 +0100

    Aggiunta funzione per richiesta al server

--- js/main.js
+++ js/main.js
@@ -11,7 +11,9 @@ const { createApp } = Vue
     created() {
         for (let i=0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            
+            .then((response) => {
+                this.mails.push(response.data.response)
+            })
         }
     }
   }).mount('#app')
