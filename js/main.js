e465565e12a3de276ea32058bd87d38b1cc185fe
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-11-16 17:09:44 +0100

    Aggiunto API all'interno di un ciclo for

--- js/main.js
+++ js/main.js
@@ -8,7 +8,10 @@ const { createApp } = Vue
         mails : []
       }
     },
-   
-
+    created() {
+        for (let i=0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            
+        }
     }
   }).mount('#app')
